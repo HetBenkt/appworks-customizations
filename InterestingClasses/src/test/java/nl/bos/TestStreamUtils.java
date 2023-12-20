7b9a55c9fd5ddb6371fbf9869a3adae2241d430f
Antal Bos <antal@bos-ictservices.nl>
2023-12-20 23:45:13 +0100

    StreamUtils compress and decompress

---
+++ InterestingClasses/src/test/java/nl/bos/TestStreamUtils.java
@@ -0,0 +1,41 @@
+package nl.bos;
+
+import com.opentext.cordys.entityCore.elements.historyElement.HistoryLog;
+import com.opentext.cordys.entityCore.utilities.stream.StreamFactory;
+import com.opentext.cordys.entityCore.utilities.stream.StreamUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+
+class TestStreamUtils {
+
+    private String actual = "<History><CD e=\"080027f7a5dca1eea7e061c773fcb579\" i=\"080027f7a5dca1eea7e061c773fcb579.1\" t=\"project-1\" v=\"ca15687458733325a316150d5a63e1ed\" x=\"080027f7a5dca1eea7e061c773fcb579.1.\"/></History>";
+
+    @Test
+    void doDeflateTest() {
+        byte[] compress = StreamUtils.compress(actual, getStreamCompressionFactory(HistoryLog.CompressionType.Deflate));
+        byte[] decompress = StreamUtils.decompress(compress, getStreamDecompressionFactory(HistoryLog.CompressionType.Deflate));
+        String expected = new String(decompress, StandardCharsets.UTF_8);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void doGZipTest() {
+        byte[] compress = StreamUtils.compress(actual, getStreamCompressionFactory(HistoryLog.CompressionType.GZip));
+        byte[] decompress = StreamUtils.decompress(compress, getStreamDecompressionFactory(HistoryLog.CompressionType.GZip));
+        String expected = new String(decompress, StandardCharsets.UTF_8);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Function<InputStream, InputStream> getStreamDecompressionFactory(HistoryLog.CompressionType compressionType) {
+        return compressionType == HistoryLog.CompressionType.Deflate ? StreamFactory::newInflaterInputStream : StreamFactory::newGZIPInputStream;
+    }
+
+    private static Function<OutputStream, OutputStream> getStreamCompressionFactory(HistoryLog.CompressionType compressionType) {
+        return compressionType == HistoryLog.CompressionType.Deflate ? StreamFactory::newDeflaterOutputStream : StreamFactory::newGZIPOutputStream;
+    }
+}
