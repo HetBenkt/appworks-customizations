24e782683e1034ca8eff68ae5c101155ef2eb4d2
Antal Bos <antal@bos-ictservices.nl>
2023-12-20 23:48:02 +0100

    Refactor

--- InterestingClasses/src/test/java/nl/bos/TestStreamUtils.java
+++ InterestingClasses/src/test/java/nl/bos/TestStreamUtils.java
@@ -13,22 +13,22 @@ import java.util.function.Function;
 
 class TestStreamUtils {
 
-    private String actual = "<History><CD e=\"080027f7a5dca1eea7e061c773fcb579\" i=\"080027f7a5dca1eea7e061c773fcb579.1\" t=\"project-1\" v=\"ca15687458733325a316150d5a63e1ed\" x=\"080027f7a5dca1eea7e061c773fcb579.1.\"/></History>";
+    private final static String ACTUAL = "<History><CD e=\"080027f7a5dca1eea7e061c773fcb579\" i=\"080027f7a5dca1eea7e061c773fcb579.1\" t=\"project-1\" v=\"ca15687458733325a316150d5a63e1ed\" x=\"080027f7a5dca1eea7e061c773fcb579.1.\"/></History>";
 
     @Test
     void doDeflateTest() {
-        byte[] compress = StreamUtils.compress(actual, getStreamCompressionFactory(HistoryLog.CompressionType.Deflate));
+        byte[] compress = StreamUtils.compress(ACTUAL, getStreamCompressionFactory(HistoryLog.CompressionType.Deflate));
         byte[] decompress = StreamUtils.decompress(compress, getStreamDecompressionFactory(HistoryLog.CompressionType.Deflate));
         String expected = new String(decompress, StandardCharsets.UTF_8);
-        Assertions.assertThat(actual).isEqualTo(expected);
+        Assertions.assertThat(ACTUAL).isEqualTo(expected);
     }
 
     @Test
     void doGZipTest() {
-        byte[] compress = StreamUtils.compress(actual, getStreamCompressionFactory(HistoryLog.CompressionType.GZip));
+        byte[] compress = StreamUtils.compress(ACTUAL, getStreamCompressionFactory(HistoryLog.CompressionType.GZip));
         byte[] decompress = StreamUtils.decompress(compress, getStreamDecompressionFactory(HistoryLog.CompressionType.GZip));
         String expected = new String(decompress, StandardCharsets.UTF_8);
-        Assertions.assertThat(actual).isEqualTo(expected);
+        Assertions.assertThat(ACTUAL).isEqualTo(expected);
     }
 
     private static Function<InputStream, InputStream> getStreamDecompressionFactory(HistoryLog.CompressionType compressionType) {
